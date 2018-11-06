ff08e85d0d84562d9cf0eb1fff63e677370428ab
HughParsonage <hugh.parsonage@gmail.com>
2018-11-07 03:37:26 +1100

    Add coverage

--- tests/testthat/test-which_true_onwards.R
+++ tests/testthat/test-which_true_onwards.R
@@ -3,4 +3,10 @@ context("test-which_true_onwards")
 test_that("which_true_onwards works", {
   expect_identical(which_true_onwards(c(TRUE, TRUE, FALSE, TRUE, TRUE)),
                    4L)
+  expect_identical(which_true_onwards(c(TRUE, FALSE)),
+                   0L)
+  expect_identical(which_true_onwards(c(TRUE, TRUE)),
+                   1L)
+  expect_identical(which_true_onwards(c(FALSE)),
+                   0L)
 })
