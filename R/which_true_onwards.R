318769654c5ca5774668fb47e262a0c0e834dd8f
HughParsonage <hugh.parsonage@gmail.com>
2018-11-07 03:24:56 +1100

    Fix which_true_onwards assertion and unit test

--- R/which_true_onwards.R
+++ R/which_true_onwards.R
@@ -2,6 +2,12 @@
 #' @param x A logical vector
 
 which_true_onwards <- function(x) {
-  stopifnot(is.logical(x), anyNA(x))
+  if (!is.logical(x)) {
+    stop("`x` was type ", class(x), ". `x` must be a logical vector.")
+  }
+  if (anyNA(x)) {
+    stop("`x` had missing values. This is not permitted.")
+  }
+
   do_which_true_onwards(x)
 }
--- tests/testthat/test-which_true_onwards.R
+++ tests/testthat/test-which_true_onwards.R
@@ -1,6 +1,6 @@
 context("test-which_true_onwards")
 
-test_that("multiplication works", {
+test_that("which_true_onwards works", {
   expect_identical(which_true_onwards(c(TRUE, TRUE, FALSE, TRUE, TRUE)),
                    4L)
 })
