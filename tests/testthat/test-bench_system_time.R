112047c21a2c4178e49081c85298bd9be5be9968
HughParsonage <hugh.parsonage@gmail.com>
2019-01-11 21:49:07 +1100

    Test bench time

---
+++ tests/testthat/test-bench_system_time.R
@@ -0,0 +1,8 @@
+context("test-bench_system_time")
+
+test_that("bench_system_time covg", {
+  skip_if_not_installed("bench")
+  x <- rep_len(c(0, -0.5, 1.2), 5e6)
+  bench_x <- bench::system_time(pminC(x, 0))
+  expect_lt(as.numeric(bench_x), 1)
+})
