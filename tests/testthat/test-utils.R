dd8eb0d50c5503066c5fc820ce144a5812b58467
HughParsonage <hugh.parsonage@gmail.com>
2019-01-11 16:54:07 +1100

    Add check_TF

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,9 @@
+context("test-utils")
+
+test_that("checkTF works", {
+  expect_null(check_TF(TRUE))
+  expect_null(check_TF(FALSE))
+  expect_error(check_TF(NA), "NA")
+  expect_error(check_TF(1:2 > 0), "length")
+  expect_error(check_TF(1:2), "integer")
+})
