31d932ef2fc8e8de42e8bdf8279587f4bdc09fc6
HughParsonage <hugh.parsonage@gmail.com>
2019-01-15 01:27:22 +1100

    Add examples, fix grammar

--- R/which_first.R
+++ R/which_first.R
@@ -1,12 +1,29 @@
-#' Where does a logical expression first occur?
+#' Where does a logical expression first return \code{TRUE}?
+#' @description A faster and safer version of \code{which.max} applied
+#' to simple to parse logical expressions.
+#'
 #' @param expr An expression, such as \code{x == 2}.
 #' @return The same as \code{which.max(expr)} or \code{which(expr)[1]} but returns \code{0L}
-#'  when \code{expr} has not \code{TRUE} values.
+#'  when \code{expr} has no \code{TRUE} values.
 #'
 #' @details
-#' The performance benefit of using this function is when the RHS of
-#' \code{expr} is a length-one numeric and the resultant logical
-#' expression is lengthy and expensive.
+#' If the \code{expr} is of the form \code{LHS <operator> RHS}
+#' and \code{LHS} is a single symbol, \code{operator} is one of
+#' \code{==},  \code{!=}, \code{>}, \code{>=}, \code{<}, or \code{<=},
+#' and \code{RHS} is a single numeric value, then \code{expr} is not
+#' evaluated directed; instead each element of \code{LHS} is compared
+#' individually.
+#'
+#' If \code{expr} is not of the above form, then \code{expr} is evaluated
+#' and passed to \code{which.max}.
+#'
+#' Using this function can be significantly faster when the computation
+#' of \code{expr} would be expensive. (The difference is only likely to
+#' be clear when \code{length(x)} is much larger than 10 million.)
+#' But even for smaller vectors, it has the benefit of returning
+#' \code{0L} if none of the values in \code{expr} are \code{TRUE}, unlike
+#' \code{which.max}.
+#'
 #'
 #' @examples
 #'
@@ -16,17 +33,27 @@
 #'   N <- 1e8
 #' }
 #'
+#' # Two examples, from slowest to fastest,
+#' # run with N = 1e8 elements
+#'
+#'                                        # seconds
 #' x <- rep_len(runif(1e4, 0, 6), N)
 #' bench_system_time(x > 5)
-#' bench_system_time(which(x > 5))
-#' bench_system_time(which.max(x > 5))
-#' bench_system_time(which_first(x > 5))
+#' bench_system_time(which(x > 5))        # 0.8
+#' bench_system_time(which.max(x > 5))    # 0.3
+#' bench_system_time(which_first(x > 5))  # 0.000
 #'
+#' ## Worst case: have to check all N elements
 #' x <- double(N)
 #' bench_system_time(x > 0)
-#' bench_system_time(which(x > 0))
-#' bench_system_time(which.max(x > 0))
-#' bench_system_time(which_first(x > 0))
+#' bench_system_time(which(x > 0))        # 1.0
+#' bench_system_time(which.max(x > 0))    # 0.4  but returns 1, not 0
+#' bench_system_time(which_first(x > 0))  # 0.1
+#'
+#' x <- as.character(x)
+#' # bench_system_time(which(x == 5))     # 2.2
+#' bench_system_time(which.max(x == 5))   # 1.6
+#' bench_system_time(which_first(x == 5)) # 1.3
 #'
 #' @export
 
