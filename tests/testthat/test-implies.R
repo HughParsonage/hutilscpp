b6dbec03765951b7b42035219c031de4319b3e23
HughParsonage <hugh.parsonage@gmail.com>
2019-01-16 15:04:47 +1100

    Add coverage

--- tests/testthat/test-implies.R
+++ tests/testthat/test-implies.R
@@ -1,5 +1,9 @@
 context("test-implies")
 
+test_that("Error handling", {
+  expect_error(Implies(1:5 > 0, 1:4 > 0), "lengths.*differ")
+})
+
 test_that("implies same as hutils", {
   skip_if_not_installed("hutils")
   skip_if_not_installed("data.table")
@@ -14,4 +18,8 @@ test_that("implies same as hutils", {
   DT[, hutils_orig := implies(x, y)]
   DT[, hutils_cpp := Implies(x, y)]
   expect_identical(DT$hutils_cpp, DT$hutils_cpp)
+})
+
+test_that("No NAs", {
+  expect_true(Implies(FALSE, FALSE, anyNAx = TRUE, anyNAy = TRUE))
 })
