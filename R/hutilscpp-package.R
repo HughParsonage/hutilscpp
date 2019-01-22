fa605af3c2b94659f06fa1b6f1cac80e266b0b76
HughParsonage <hugh.parsonage@gmail.com>
2019-01-23 00:04:06 +1100

    CRAN NOTE avoidance

--- R/hutilscpp-package.R
+++ R/hutilscpp-package.R
@@ -1,5 +1,7 @@
 #' @keywords internal
 #' @import Rcpp
-#' @importFrom data.table as.data.table
+#' @import data.table
+#' @importFrom utils hasName
+
 #' @useDynLib hutilscpp, .registration=TRUE
 "_PACKAGE"
