b0a95d77a6bc647959ee822f44e2d050415f7a95
HughParsonage <hugh.parsonage@gmail.com>
2019-02-11 01:22:05 +1100

    Test coverage

--- R/hutilscpp-package.R
+++ R/hutilscpp-package.R
@@ -4,6 +4,7 @@
 #' @importFrom utils hasName
 #' @importFrom utils head
 #' @importFrom hutils NOR
+#' @importFrom hutils XOR
 #' @importFrom hutils AND
 
 #' @useDynLib hutilscpp, .registration=TRUE
--- tests/testthat/test-is_constant.R
+++ tests/testthat/test-is_constant.R
@@ -5,12 +5,14 @@ test_that("is_constant works", {
   expect_true(is_constant(integer(0)))
   expect_true(is_constant(integer(1)))
 
+  expect_true(is_constant(logical(10)))
   expect_true(is_constant(integer(10)))
   expect_true(is_constant(double(10)))
   expect_true(is_constant(character(10)))
   expect_true(is_constant(as.factor(character(10))))
   expect_true(is_constant(raw(10)))
 
+  expect_false(is_constant(c(TRUE, FALSE)))
   expect_false(is_constant(c(integer(10), 1L)))
   expect_false(is_constant(c(integer(10), 1)))
   expect_false(is_constant(c(character(10), 1)))
@@ -18,4 +20,13 @@ test_that("is_constant works", {
   expect_false(is_constant(c(raw(10), as.raw(1))))
   expect_false(is_constant(c(factor(c(character(10), 1)))))
 
+})
+
+test_that("is_constant with NA", {
+  expect_false(is_constant(c(TRUE, TRUE, TRUE, NA)))
+  expect_false(is_constant(c(FALSE, FALSE, NA)))
+  expect_false(is_constant(c(1L, 1L, 1L, NA)))
+  expect_true(is_constant(c(NA, NA, NA)))
+  expect_true(is_constant(c(NA_integer_, NA_integer_)))
+  expect_true(is_constant(c(NA_character_, NA_integer_)))
 })
