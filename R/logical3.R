fb6b8472e1bac3a69cbb7c064270338aab176ec9
HughParsonage <hugh.parsonage@gmail.com>
2019-04-20 23:48:46 +1000

    Rename to logical3

--- R/logical3.R
+++ R/logical3.R
@@ -1,4 +1,5 @@
 #' Vectorized logical with support for short-circuits
+#' @name logical3
 #' @param x,y,z Logical vectors. If \code{z} is \code{NULL} it is interpreted as \code{TRUE} or
 #' \code{FALSE} recycled to the length of \code{x}.
 #' @export and3 or3
@@ -17,6 +18,7 @@ and3 <- function(x, y, z = NULL) {
   do_and3(x, y, z)
 }
 
+#' @rdname logical3
 or3 <- function(x, y, z = NULL) {
   if (length(x) != length(y)) {
     stop("`length(x) = ",
