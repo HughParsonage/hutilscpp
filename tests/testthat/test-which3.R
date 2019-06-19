eb8550e2a72c7f4ea3947b5b5d206bd2d2d53685
HughParsonage <hugh.parsonage@gmail.com>
2019-06-19 12:23:54 +1000

    Add coverage

--- tests/testthat/test-which3.R
+++ tests/testthat/test-which3.R
@@ -9,3 +9,16 @@ test_that("which3 works", {
                       And = FALSE),
                1:3)
 })
+
+
+test_that("which3 prepare", {
+  x <- rep(TRUE, 10)
+  y <- 1:10 %in% c(1, 2, 3, 10)
+  z <- 1:10 %in% c(2, 3, 10)
+  expect_equal(which3(x, y, z), which(x & y & z))
+  expect_equal(which3(x, y, z, prepare = TRUE), which(x & y & z))
+  expect_equal(do_which3_prepare1(which(z), y, x), which(x & y & z))
+
+
+})
+
