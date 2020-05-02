f00e3f58b08b521b6cd5c87e57f83eb6ff067dc0
HughParsonage <hugh.parsonage@gmail.com>
2020-05-02 16:56:20 +1000

    Initial commit of omp040 Support for parallel

---
+++ R/logical3s.R
@@ -0,0 +1,384 @@
+#' Complex logical expressions
+#' @name logical3s
+#' @description Performant implementations of \code{&} et \code{or}.
+#' Performance is high when the expressions are long (i.e. over 10M elements)
+#' and in particular when they are of the form \code{lhs <op> rhs} for binary
+#' \code{<op>}.
+#' @param exprA,exprB,exprC Expressions of the form \code{x <op> y}.
+#' with \code{<op>} one of the standard binary operators.
+#'
+#'
+#'
+#' @return
+#'
+#' \code{and3s} and \code{or3s} return \code{exprA & exprB & exprC} and
+#' \code{exprA | exprB | exprC} respectively. If any expression is missing
+#' it is considered \code{TRUE} for \code{and3s} and \code{FALSE} for \code{or3s};
+#' in other words only the results of the other expressions count towards the result.
+#'
+#' @export
+
+op2M <- function(operator) {
+  match(operator,
+        c("!=", "==", ">=", "<=", ">", "<", "%in%", "%between%",
+          "%(between)%", "%]between[%"),
+        nomatch = 0L)
+}
+
+is_binary_sexp <- function(sexprA, .parent_nframes = 2L) {
+  # e.g.
+  #      x > -1L
+  isBinary <-
+    is.call(sexprA) &&
+    length(sexprA) == 3L &&
+    (M <- op2M(as.character(sexprA[[1L]]))) &&
+    is.name(lhs <- sexprA[[2L]])
+
+
+  if (isBinary) {
+    # c(1, 2)  gives rhs a language object, but rhs_eval is length-2 numeric
+    # lhs <- sexprA[[2]]
+    rhs <- sexprA[[3]]
+
+    # lhs_eval <- eval.parent(lhs)
+
+
+    attr(isBinary, "M") <- M
+    attr(isBinary, "lhs") <- lhs
+    attr(isBinary, "rhs") <- rhs
+    attr(isBinary, "rhs_eval") <- rhs_eval <- eval.parent(rhs, n = .parent_nframes)
+
+    if (OR(is.numeric(rhs),
+           AND(is.numeric(rhs_eval),
+               OR(length(rhs_eval) == 1L,
+                  OR(M == op2M("%between%") && length(rhs_eval) == 2L,
+                     M == op2M("%in%")))))) {
+      attr(isBinary, "rhs_eval") <- rhs_eval
+      return(isBinary)
+    }
+  }
+  FALSE
+}
+
+is_lgl_sym <- function(sexpr, expr) {
+  # Symbol first since is.logical will evaluate
+  # Keep expr from evaluating so that the call can
+  # be separated
+  is.symbol(sexpr) && is.logical(expr)
+}
+
+is_lgl_negation <- function(sexpr, expr) {
+  is.call(sexpr) && as.character(sexpr)[[1]] == "!" &&
+    length(sexpr) > 1L &&
+    is.symbol(sexpr[[2]])
+}
+
+#' @rdname logical3
+#' @param exprA,exprB,exprC Expressions which would be logical expressions
+#' if evaluated.
+#' @param ... Further expressions.
+#' @param .parent_nframes \describe{
+#' \item{\code{integer(1)}}{For internal use. Passed to \code{eval.parent}.}
+#' }
+#' @return Logical vector same length as \code{exprA}. Equivalent to
+#' \code{exprA & exprB & exprC}.
+#' @export
+and3s <- function(exprA, exprB, exprC, ..., .parent_nframes = 1L,
+                  nThreads = getOption("hutilscpp.nThreads", 1L)) {
+  sexprA <- substitute(exprA)
+  sexprB <- substitute(exprB)
+  sexprC <- substitute(exprC)
+
+  X3 <- Y3 <- Z3 <- integer(0)
+  A <- B <- C <- logical(0)
+  x <- y <- z <- integer(0)
+  ox <- oy <- oz <- -1L
+  x1 <- y1 <- z1 <- 0L
+  x2 <- y2 <- z2 <- 0L
+
+  is_seq <- function(A) {
+    identical(A, seq.int(A[1L], along.with = A))
+  }
+
+  isBinaryA <- is_binary_sexp(sexprA)
+  if (isBinaryA) {
+    x <- eval.parent(sexprA[[2]], n = .parent_nframes)
+    ox <- attr(isBinaryA, "M")
+    rhs_eval <- attr(isBinaryA, "rhs_eval")
+    if (ox == op2M("%between%")) { # between so two elements
+      x1 <- rhs_eval[[1]]
+      x2 <- rhs_eval[[2]]
+    } else if (ox == op2M("%in%")) {
+      X3 <- rhs_eval
+
+      x1 <- X3[1]
+      if (is_seq(X3)) {
+        x2 <- X3[length(X3)]
+        ox <- op2M("%between%")
+      } else {
+        if (length(X3) > 100L) {
+          A <- x %in% X3
+        } else {
+          A <- do_par_in(x, X3)
+        }
+        x <- integer(0)
+        ox <- -1L
+        x2 <- X3[2]
+      }
+    } else {
+      x1 <- x2 <- attr(isBinaryA, "rhs_eval")
+    }
+
+  } else if (is_lgl_negation(sexprA, exprA)) {
+    A <- eval.parent(sexprA[[2]])
+    ox <- 1L
+  } else {
+    A <- exprA
+  }
+  if (!missing(exprB)) {
+    isBinaryB <- is_binary_sexp(sexprB)
+    if (isBinaryB) {
+      y <- eval.parent(sexprB[[2]], n = .parent_nframes)
+      oy <- attr(isBinaryB, "M")
+      rhs_eval <- attr(isBinaryB, "rhs_eval")
+      if (oy == op2M("%between%")) { # between so two elements
+        y1 <- rhs_eval[[1]]
+        y2 <- rhs_eval[[2]]
+      } else if (oy == op2M("%in%")) {
+        Y3 <- rhs_eval
+        stopifnot(length(Y3) > 1)
+        y1 <- Y3[1L]
+        if (is_seq(Y3)) {
+          # Treat x %in% a:b as x %between% c(a, b)
+          y2 <- Y3[length(Y3)]
+          oy <- op2M("%between%")
+        } else {
+          if (length(Y3) > 100L) {
+            B <- y %in% Y3
+          } else {
+            B <- do_par_in(y, Y3, nThread = nThread)
+          }
+          y <- integer(0)
+          oy <- -1L
+          y2 <- Y3[2]
+        }
+      } else {
+        y1 <- y2 <- rhs_eval
+      }
+
+    } else if (is_lgl_negation(sexprB, exprB)) {
+      B <- eval.parent(sexprB[[2]])
+      oy <- 1L
+    } else {
+      B <- exprB
+    }
+  }
+
+
+  if (!missing(exprC)) {
+    isBinaryC <- is_binary_sexp(sexprC)
+
+    if (isBinaryC) {
+      z <- eval.parent(sexprC[[2]], n = .parent_nframes)
+      oz <- attr(isBinaryC, "M")
+      rhs_eval <- attr(isBinaryC, "rhs_eval")
+      if (oz == op2M("%between%")) { # between so two elements
+        z1 <- rhs_eval[[1]]
+        z2 <- rhs_eval[[2]]
+      } else if (oz == op2M("%in%")) {
+        Z3 <- rhs_eval
+        stopifnot(length(Z3) > 1)
+        z1 <- Z3[1]
+        if (is_seq(Z3)) {
+          z2 <- Z3[length(Z3)]
+          oz <- op2M("%between%")
+        } else {
+          if (length(Z3) > 100L) {
+            C <- z %in% Z3
+          } else {
+            C <- do_par_in(z, Z3, nThread = nThread)
+          }
+          z <- integer(0L)
+          oz <- -1L
+        }
+      } else {
+        z1 <- z2 <- rhs_eval
+      }
+
+    } else if (is_lgl_negation(sexprC, exprC)) {
+      C <- eval.parent(sexprC[[2]], n = .parent_nframes)
+      oz <- 1L
+    } else {
+      C <- exprC
+    }
+  }
+
+  ans <-
+    do_and3_par(x, ox, x1, x2,
+                y, oy, y1, y2,
+                z, oz, z1, z2,
+                A, B, C,
+                deparse(sexprA),
+                nThread)
+
+  if (missing(..1)) {
+    return(ans)
+  }
+
+  and3s(ans, ..., .parent_nframes = .parent_nframes + 1L, nThread = nThread)
+}
+
+#' @rdname logical3
+#' @export
+
+or3s <- function(exprA, exprB, exprC, ..., .parent_nframes = 1L,
+                 nThread = getOption("hutilscpp.nThread", 1L)) {
+  sexprA <- substitute(exprA)
+  sexprB <- substitute(exprB)
+  sexprC <- substitute(exprC)
+
+  X3 <- Y3 <- Z3 <- integer(0)
+  A <- B <- C <- logical(0)
+  x <- y <- z <- integer(0)
+  ox <- oy <- oz <- -1L
+  x1 <- y1 <- z1 <- 0L
+  x2 <- y2 <- z2 <- 0L
+
+  is_seq <- function(A) {
+    identical(A, seq.int(A[1L], along.with = A))
+  }
+
+  isBinaryA <- is_binary_sexp(sexprA)
+  if (isBinaryA) {
+    x <- eval.parent(sexprA[[2]], n = .parent_nframes)
+    ox <- attr(isBinaryA, "M")
+    rhs_eval <- attr(isBinaryA, "rhs_eval")
+    if (ox == op2M("%between%")) { # between so two elements
+      x1 <- rhs_eval[[1]]
+      x2 <- rhs_eval[[2]]
+    } else if (ox == op2M("%in%")) {
+      X3 <- rhs_eval
+
+      x1 <- X3[1]
+      if (is_seq(X3)) {
+        x2 <- X3[length(X3)]
+        ox <- op2M("%between%")
+      } else {
+        if (length(X3) > 100L) {
+          A <- x %in% X3
+        } else {
+          A <- do_par_in(x, X3)
+        }
+        x <- integer(0)
+        ox <- -1L
+        x2 <- X3[2]
+      }
+    } else {
+      x1 <- x2 <- attr(isBinaryA, "rhs_eval")
+    }
+
+  } else if (is_lgl_negation(sexprA, exprA)) {
+    A <- eval.parent(sexprA[[2]])
+    ox <- 1L
+  } else {
+    A <- exprA
+  }
+  if (!missing(exprB)) {
+    isBinaryB <- is_binary_sexp(sexprB)
+    if (isBinaryB) {
+      y <- eval.parent(sexprB[[2]], n = .parent_nframes)
+      oy <- attr(isBinaryB, "M")
+      rhs_eval <- attr(isBinaryB, "rhs_eval")
+      if (oy == op2M("%between%")) { # between so two elements
+        y1 <- rhs_eval[[1]]
+        y2 <- rhs_eval[[2]]
+      } else if (oy == op2M("%in%")) {
+        Y3 <- rhs_eval
+        stopifnot(length(Y3) > 1)
+        y1 <- Y3[1L]
+        if (is_seq(Y3)) {
+          # Treat x %in% a:b as x %between% c(a, b)
+          y2 <- Y3[length(Y3)]
+          oy <- op2M("%between%")
+        } else {
+          if (length(Y3) > 100L) {
+            B <- y %in% Y3
+          } else {
+            B <- do_par_in(y, Y3)
+          }
+          y <- integer(0)
+          oy <- -1L
+          y2 <- Y3[2]
+        }
+      } else {
+        y1 <- y2 <- rhs_eval
+      }
+
+    } else if (is_lgl_negation(sexprB, exprB)) {
+      # Make coverage explicit
+      if (.parent_frame > 1L) {
+        B <- eval.parent(sexprB[[2]], n = .parent_nframes)
+      } else {
+        B <- eval.parent(sexprB[[2]], n = .parent_nframes)
+      }
+      oy <- 1L
+    } else {
+      B <- exprB
+    }
+  }
+
+
+  if (!missing(exprC)) {
+    isBinaryC <- is_binary_sexp(sexprC)
+
+    if (isBinaryC) {
+      z <- eval.parent(sexprC[[2]], n = .parent_nframes)
+      oz <- attr(isBinaryC, "M")
+      rhs_eval <- attr(isBinaryC, "rhs_eval")
+      if (oz == op2M("%between%")) { # between so two elements
+        z1 <- rhs_eval[[1]]
+        z2 <- rhs_eval[[2]]
+      } else if (oz == op2M("%in%")) {
+        Z3 <- rhs_eval
+        stopifnot(length(Z3) > 1)
+        z1 <- Z3[1]
+        if (is_seq(Z3)) {
+          z2 <- Z3[length(Z3)]
+          oz <- op2M("%between%")
+        } else {
+          if (length(Z3) > 100L) {
+            C <- z %in% Z3
+          } else {
+            C <- do_par_in(z, Z3)
+          }
+          z <- integer(0L)
+          oz <- -1L
+        }
+      } else {
+        z1 <- z2 <- rhs_eval
+      }
+
+    } else if (is_lgl_negation(sexprC, exprC)) {
+      C <- eval.parent(sexprC[[2]], n = .parent_nframes)
+      oz <- 1L
+    } else {
+      C <- exprC
+    }
+  }
+  ans <-
+    do_or3_par(x, ox, x1, x2,
+               y, oy, y1, y2,
+               z, oz, z1, z2,
+               A, B, C,
+               deparse(sexprA),
+               nThread = nThread)
+  if (missing(..1)) {
+    return(ans)
+  }
+  if (missing(..2)) {
+    return(ans | ..1)
+  }
+  or3s(ans, ..., .parent_nframes = .parent_nframes + 1L, nThread = nThread)
+}
+
+
---
+++ src/do_and3s.cpp
@@ -0,0 +1,513 @@
+#include "cppdiffuprating.h"
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export(rng = false)]]
+LogicalVector do_and3_par(IntegerVector x, int ox, int x1, int x2,
+                          IntegerVector y, int oy, int y1, int y2,
+                          IntegerVector z, int oz, int z1, int z2,
+                          LogicalVector A,
+                          LogicalVector B,
+                          LogicalVector C,
+                          CharacterVector nom,
+                          int nThread = 1) {
+  int nx = x.length();
+  int nA = A.length();
+  int n = (nx > nA) ? nx : nA;
+  bool useX = x.length() == n;
+  bool useY = y.length() == n;
+  bool useZ = z.length() == n;
+
+  // Which variables are bare logicals
+  bool A_lgl = A.length() == n;
+  bool B_lgl = B.length() == n;
+  bool C_lgl = C.length() == n;
+
+  // Is the 1st, 2nd, 3rd expression usable or should we just set it to false?
+  bool e1 = useX || A_lgl;
+  bool e2 = useY || B_lgl;
+  bool e3 = useZ || C_lgl;
+
+  // Are the expressions preceded by `!` -- i.e the opposite
+  bool A_opposite = A_lgl && ox == 1;
+  bool B_opposite = B_lgl && oy == 1;
+  bool C_opposite = C_lgl && oz == 1;
+
+  if (useX && A_lgl) {
+    stop("Internal error: useX && A_lgl");
+  }
+  if (useY && B_lgl) {
+    stop("Internal error: useY && B_lgl");
+  }
+  if (useZ && C_lgl) {
+    stop("Internal error: useZ && C_lgl");
+  }
+
+  if (!e1 && !e2 && !e3) {
+    return LogicalVector(n);
+  }
+
+  LogicalVector out = no_init(n);
+
+  if (useX && useY && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (useX && useY && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (useX && useY && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        C[i];
+    }
+    return out;
+  }
+
+  // e3 = false
+  if (useX && useY && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        single_ox_x1_x2(y[i], oy, y1, y2);
+    }
+    return out;
+  }
+
+  // B_lgl (but B_opposite must always precede!)
+  if (useX && B_opposite && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        !B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (useX && B_lgl && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (useX && B_opposite && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        !B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (useX && B_lgl && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (useX && B_opposite && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        !B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (useX && B_lgl && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (useX && B_opposite && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        !B[i];
+    }
+    return out;
+  }
+
+  if (useX && B_lgl && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2) &&
+        B[i];
+    }
+    return out;
+  }
+
+  if (useX && !e2 && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        single_ox_x1_x2(x[i], ox, x1, x2);
+    }
+    return out;
+  }
+  // // A_opposite
+
+  if (A_opposite && useY && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_opposite && useY && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && useY && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        C[i];
+    }
+    return out;
+  }
+
+  // e3 = false
+  if (A_opposite && useY && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2);
+    }
+    return out;
+  }
+
+  // B_lgl
+  if (A_opposite && B_opposite && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        !B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_opposite && B_lgl && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_opposite && B_opposite && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        !B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && B_lgl && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && B_opposite && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        !B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && B_lgl && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && B_opposite && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        !B[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && B_lgl && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i] &&
+        B[i];
+    }
+    return out;
+  }
+
+  if (A_opposite && !e2 && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        !A[i];
+    }
+    return out;
+  }
+
+  // // A_lgl
+
+  if (A_lgl && useY && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_lgl && useY && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && useY && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2) &&
+        C[i];
+    }
+    return out;
+  }
+
+  // e3 = false
+  if (A_lgl && useY && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        single_ox_x1_x2(y[i], oy, y1, y2);
+    }
+    return out;
+  }
+
+  // B_lgl
+  if (A_lgl && B_opposite && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        !B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_lgl && B_lgl && useZ) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        B[i] &&
+        single_ox_x1_x2(z[i], oz, z1, z2);
+    }
+    return out;
+  }
+
+  if (A_lgl && B_opposite && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        !B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && B_lgl && C_opposite) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        B[i] &&
+        !C[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && B_opposite && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        !B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && B_lgl && C_lgl) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        B[i] &&
+        C[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && B_opposite && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        !B[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && B_lgl && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i] &&
+        B[i];
+    }
+    return out;
+  }
+
+  if (A_lgl && !e2 && !e3) {
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      out[i] =
+        A[i];
+    }
+    return out;
+  }
+
+#pragma omp parallel for
+  for (int i = 0; i < n; ++i) {
+    bool oi = false;
+
+    // 1st expression
+    if (e1) {
+      oi = useX ? single_ox_x1_x2(x[i], ox, x1, x2) : (A_opposite ? !A[i] : A[i]);
+      if (!oi) {
+        out[i] = FALSE;
+        continue;
+      }
+    }
+
+    // 2nd expression
+    // #nocov start
+    if (e2) {
+      oi = useY ? single_ox_x1_x2(y[i], oy, y1, y2) : (B_opposite ? !B[i] : B[i]);
+      if (!oi) {
+        out[i] = FALSE;
+        continue;
+      }
+    }
+    // #nocov end
+    // 3rd expression
+    if (e3) {
+      oi = useZ ? single_ox_x1_x2(z[i], oz, z1, z2) : (C_opposite ? !C[i] : C[i]);
+      if (!oi) {
+        out[i] = FALSE;
+        continue;
+      }
+    }
+
+    out[i] = TRUE;
+
+  }
+  return out;
+}
+
+
