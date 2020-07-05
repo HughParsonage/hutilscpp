d58509ee2ee7e5147151197b5e9404498fd2cf7b
HughParsonage <hugh.parsonage@gmail.com>
2020-07-06 03:01:52 +1000

    More coverage

--- tests/testthat/test-sum_and3s.R
+++ tests/testthat/test-sum_and3s.R
@@ -6,18 +6,40 @@ test_that("sum_and3s works", {
       sum_band3(exprA & exprB, exprC, ...)
     }
   }
-  abc <- 1:100
-  def <- 1:100
-  ghi <- 1:100
+  abc <- -1:100
+  def <- -1:100
+  ghi <- -1:100
   x <- abc == 1
   expect_equal(sum_and3s(x, def >= 1, ghi <= 2), 1)
+  expect_equal(sum_and3s(!x, def >= 1, ghi <= 2), 1)
+  expect_equal(sum_and3s(!x, def >= 1, ghi >= 2), 99)
   expect_equal(sum_and3s(x, x, x), 1)
+  expect_equal(sum_and3s(!x, x, x), 0)
+  ox <- 50L
+  expect_equal(sum_and3s(!x, !x, abc == ox), 1)
+  expect_equal(sum_and3s(!x, !x, !x), length(x) - 1)
 
   expect_equal(sum_and3s(abc == 1, def >= 1, ghi <= 2), 1)
 
   DT <- data.table(A = 1:100, B = 2L, Z = 3L)
   expect_equal(DT[, sum_and3s(A %in% c(5L, 5L, 6L), B == 2L, Z >= 1L)],
                DT[, sum_band3(A %in% c(5L, 5L, 6L), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(A %between% c(5L, 6L), B == 2L, Z >= 1L)],
+               DT[, sum_band3(A %between% c(5L, 6L), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(A %in% c(5L, 6L), B == 2L, Z >= 1L)],
+               DT[, sum_band3(A %in% c(5L, 6L), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(A %in% B, B == 2L, Z >= 1L)],
+               DT[, sum_band3(A %in% B, B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(B == 2L, A %in% B, B == 2L, Z >= 1L)],
+               DT[, sum_band3(B == 2L, A %in% B, B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(B == 2L, A %in% c(5:6, 5L), B == 2L, Z >= 1L)],
+               DT[, sum_band3(B == 2L, A %in% c(5:6, 5L), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(B == 2L, A %in% c(5:6), B == 2L, Z >= 1L)],
+               DT[, sum_band3(B == 2L, A %in% c(5:6), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(B == 2L, B == 2L, A %in% c(6:5), B == 2L, Z >= 1L)],
+               DT[, sum_band3(B == 2L, B == 2L, A %in% c(6:5), B == 2L, Z >= 1L)])
+  expect_equal(DT[, sum_and3s(B == 2L, B == 2L, A %in% B, B == 2L, Z >= 1L)],
+               DT[, sum_band3(B == 2L, B == 2L, A %in% B, B == 2L, Z >= 1L)])
 
 
 })
