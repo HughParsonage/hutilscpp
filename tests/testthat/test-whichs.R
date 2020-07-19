8f15e83c911e5eb61a18ef5e2c99d424a674ffcd
HughParsonage <hugh.parsonage@gmail.com>
2020-07-19 14:49:21 +1000

    Initial commit of whichs

---
+++ tests/testthat/test-whichs.R
@@ -0,0 +1,20 @@
+test_that("whichs works", {
+  xox <- rep_len(1:10, 2)
+  yoy <- rep_len(1:10, length(xox))
+  expect_equal(whichs(xox != 1), which(xox != 1))
+  expect_equal(whichs(xox == 1), which(xox == 1))
+  expect_equal(whichs(xox >= 1), which(xox >= 1))
+  expect_equal(whichs(xox <= 1), which(xox <= 1))
+  expect_equal(whichs(xox < 1), which(xox < 1))
+  expect_equal(whichs(xox > 1), which(xox > 1))
+  expect_equal(whichs(xox %in% 1), which(xox %in% 1))
+  expect_equal(whichs(identity(xox %in% 1)), which(identity(xox %in% 1)))
+
+
+  expect_equal(whichs(xox != yoy), which(xox != yoy))
+  expect_equal(whichs(xox == yoy), which(xox == yoy))
+  expect_equal(whichs(xox >= yoy), which(xox >= yoy))
+  expect_equal(whichs(xox <= yoy), which(xox <= yoy))
+  expect_equal(whichs(xox < yoy), which(xox < yoy))
+  expect_equal(whichs(xox > yoy), which(xox > yoy))
+})
