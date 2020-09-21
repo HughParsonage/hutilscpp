848cea2f4a7bac5d6bbf43fa46e401b40bce8538
HughParsonage <hugh.parsonage@gmail.com>
2020-09-21 23:43:32 +1000

    Coverage

--- tests/testthat/test-which_last.R
+++ tests/testthat/test-which_last.R
@@ -661,7 +661,7 @@ test_that("do_which_last_xd_ind", {
   x <- c(NA, x, x)
   expect_equal(which_last(x %in% t2),
                last_which(x %in% t2))
-  t2 <- head(t2, 99)
+  t2 <- c(head(t2, 98), NA)
   expect_equal(which_last(x %in% t2),
                last_which(x %in% t2))
 
