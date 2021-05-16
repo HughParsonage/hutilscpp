ac44087b94db73dbf7417125a86c345adeb0aa9f
HughParsonage <hugh.parsonage@gmail.com>
2021-05-17 01:00:00 +1000

    allocate and altrep now C

---
+++ R/allocate.R
@@ -0,0 +1,29 @@
+
+
+allocate0_int <- function(N, nThread = 1L) {
+  if ((res <- isnt_number(N)) || (res <- isnt_number(nThread))) {
+    stop(attr(res, "ErrorMessage"))
+  }
+  .Call("Callocate0_int", N, nThread, PACKAGE = packageName())
+}
+
+allocate0_dbl <- function(N, nThread = 1L) {
+  if ((res <- isnt_number(N)) || (res <- isnt_number(nThread))) {
+    stop(attr(res, "ErrorMessage"))
+  }
+  .Call("Callocate0_dbl", N, nThread, PACKAGE = packageName())
+}
+
+
+allocate0_except <- function(N, India, Victor, nThread = 1L) {
+  if ((res <- isnt_number(N)) || (res <- isnt_number(nThread))) {
+    stop(attr(res, "ErrorMessage"))
+  }
+  India <- ensure_integer(India)
+  Victor <- ensure_integer(Victor)
+  .Call("Callocate0_except", N, India, Victor, nThread, PACKAGE = packageName())
+}
+
+allocate_with_root <- function(N, a, r, left, do_pmin, nThread = 1L) {
+  .Call("Callocate_with_root", N, a, r, left, do_pmin, nThread, PACKAGE = packageName())
+}
--- src/allocate.c
+++ src/allocate.c
@@ -1,4 +1,4 @@
-#include "hutilscpp.h"
+#include "hutilsc.h"
 
 SEXP Callocate0_int(SEXP N, SEXP nThread) {
   if (xlength(N) != 1 || (TYPEOF(N) != INTSXP && TYPEOF(N) != REALSXP)) {
@@ -11,13 +11,115 @@ SEXP Callocate0_int(SEXP N, SEXP nThread) {
   R_xlen_t n = TYPEOF(N) == INTSXP ? asInteger(N) : asReal(N);
   int nthreads = asInteger(nThread);
   SEXP ans = PROTECT(allocVector(INTSXP, n));
-  int * restrict out = INTEGER(ans);
+
+  int * ansp = INTEGER(ans);
 #if defined _OPENMP && _OPENMP >= 201511
 #pragma omp parallel for num_threads(nthreads)
 #endif
   for (R_xlen_t i = 0; i < n; ++i) {
-    out[i] = 0;
+    ansp[i] = 0;
   }
   UNPROTECT(1);
   return ans;
+}
+
+SEXP Callocate0_dbl(SEXP N, SEXP nThread) {
+  if (xlength(N) != 1 || (TYPEOF(N) != INTSXP && TYPEOF(N) != REALSXP)) {
+    error("N not a single number.");
+  }
+  if (xlength(nThread) != 1 ||
+      (TYPEOF(nThread) != INTSXP && TYPEOF(nThread) != REALSXP)) {
+    error("nThread not a single number.");
+  }
+  R_xlen_t n = TYPEOF(N) == INTSXP ? asInteger(N) : asReal(N);
+  int nthreads = asInteger(nThread);
+  SEXP ans = PROTECT(allocVector(REALSXP, n));
+
+  double * ansp = REAL(ans);
+#if defined _OPENMP && _OPENMP >= 201511
+#pragma omp parallel for num_threads(nthreads)
+#endif
+  for (R_xlen_t i = 0; i < n; ++i) {
+    ansp[i] = 0;
+  }
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP Sallocate_with_root(R_xlen_t N,
+                         int a,
+                         R_xlen_t r,
+                         bool left,
+                         bool do_pmin,
+                         int nThread) {
+  SEXP ans = PROTECT(allocVector(INTSXP, N));
+  int * out = INTEGER(ans);
+
+#if defined _OPENMP && _OPENMP >= 201511
+#pragma omp parallel for num_threads(nThread)
+#endif
+  for (R_xlen_t i = 0; i < N; ++i) {
+    bool post = i >= r;
+    if (post != left) {
+      out[i] = a;
+      continue;
+    }
+    if (do_pmin) {
+      out[i] = post ? (r - i) : (i - r);
+    } else {
+      out[i] = post ? (i - r) : (r - i);
+    }
+    out[i] += a;
+  }
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP Callocate_with_root(SEXP NN, SEXP aa, SEXP rr, SEXP lleft, SEXP ddo_pmin, SEXP nthreads) {
+  R_xlen_t N = isReal(NN) ? asReal(NN) : asInteger(NN);
+  int a = asInteger(aa);
+  R_xlen_t r = isReal(rr) ? asReal(rr) : asInteger(rr);
+  const bool left = asLogical(lleft);
+  const bool do_pmin = asLogical(ddo_pmin);
+  int nThread = asInteger(nthreads);
+  return Sallocate_with_root(N, a, r, left, do_pmin, nThread);
+}
+
+SEXP Callocate0_except(SEXP NN, SEXP Ind, SEXP Vic, SEXP nthread) {
+  R_xlen_t N = isReal(NN) ? asReal(NN) : asInteger(NN);
+  int nThread = asInteger(nthread);
+  if (TYPEOF(Ind) != INTSXP || TYPEOF(Vic) != INTSXP) {
+    error("Internal error(Callocate0_except): wrong types"); // # nocov
+  }
+  R_xlen_t tn = xlength(Ind);
+  R_xlen_t vn = xlength(Vic);
+
+  const int * India = INTEGER(Ind);
+  const int * Victor = INTEGER(Vic);
+  SEXP ans = PROTECT(allocVector(INTSXP, N));
+  int * ansp = INTEGER(ans);
+
+#if defined _OPENMP && _OPENMP >= 201511
+#pragma omp parallel for num_threads(nThread)
+#endif
+  for (R_xlen_t i = 0; i < N; ++i) {
+    ansp[i] = 0;
+  }
+  if (vn < 1 || tn < 1) {
+    warning("Ignoring Victor."); // # nocov
+    UNPROTECT(1); // # nocov
+    return ans; // # nocov
+  }
+  for (R_xlen_t j = 0; j < tn; ++j) {
+    R_xlen_t i = India[j];
+    if (i < 0 || i >= N) {
+      continue; // # nocov
+    }
+    int v = (tn == vn) ? Victor[j] : Victor[0];
+    ansp[i] = v;
+  }
+  UNPROTECT(1);
+  return ans;
+
+
 }
