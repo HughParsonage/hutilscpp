dfa74f91d91fdba0df71140fd087f4e3ac6cd34b
HughParsonage <hugh.parsonage@gmail.com>
2021-05-24 14:44:46 +1000

    Test string equals

---
+++ tests/testthat/test_string_equals.R
@@ -0,0 +1,14 @@
+test_that("string_equals", {
+  x <- c("svGwr", "Jlvu2", "o8u2a", "ToOWY", "fXg9I", "iDc4K", "LKVok",
+         "z12V4", "VYRWX", "A0rmW")
+  y <- c("svGwr", "Jlvu2", "o8u2a", "ToOWY", "fXg9I", "iDc4K", "LKVok",
+         "z12V4", "VYRWX", "A0rmW")
+  zz <- c("svGwr", "Jlvu2", "o8u2a", "ToOWY", "fXg9I", "iDc4K", "LKVok",
+         "z12V4", "VYRWX", "A0rmW", "more")
+  zw <- c("svGwr", "Jlvu2", "o8u2a", "ToOWY", "fXg9I", "iDc4K", "LKVok",
+          "z12V4", "VYRWX", "A0rm_")
+
+  expect_true(string_equals(x, y))
+  expect_false(string_equals(x, zz))
+  expect_false(string_equals(x, zw))
+})
