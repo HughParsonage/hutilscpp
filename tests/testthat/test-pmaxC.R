02cdef37b4c2f5cee11ef43ccedea024142d25eb
HughParsonage <hugh.parsonage@gmail.com>
2021-05-24 14:25:52 +1000

    Coverage

--- tests/testthat/test-pmaxC.R
+++ tests/testthat/test-pmaxC.R
@@ -2,6 +2,7 @@ test_that("pmaxC works", {
   x <- 1:10
   expect_equal(pmaxC(x, 3L), pmax(x, 3L))
   expect_equal(pmaxC(x, 3), pmax(x, 3))
+  expect_equal(pmaxC(x, 3, dbl_ok = FALSE), pmax(x, 3))
 })
 
 test_that("pmaxC in-place", {
@@ -24,7 +25,7 @@ test_that("pmaxC error handling", {
   expect_error(pmaxC("", ""), regexp = "numeric")
   expect_error(pmaxC(list(1), 1L), regexp = "atomic")
   expect_error(pmaxC(1:5, 1:2), "length.*2")
-  expect_message(pmaxC(1:5, 1.5), "Output is double.")
+  expect_message(pmaxC(1:5, 1.5), "Output is double")
   expect_error(pmaxC(1:5, 2.5, dbl_ok = FALSE))
 })
 
