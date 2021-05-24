a11a167ace6530386581619eae1592675c9fb40e
HughParsonage <hugh.parsonage@gmail.com>
2021-05-24 14:40:29 +1000

    Test divisible2

--- tests/testthat/test-divisible.R
+++ tests/testthat/test-divisible.R
@@ -1,4 +1,5 @@
 test_that("divisible works", {
   expect_equal(divisible(1:100, 3L), (1:100) %% 3L == 0L)
+  expect_equal(divisible2(1:100), (1:100) %% 2L == 0L)
   expect_equal(divisible16(1:100), (1:100) %% 16L == 0L)
 })
