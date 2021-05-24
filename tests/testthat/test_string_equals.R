d1eccc84f99ca73c465bfafec1469fe0c9f7379a
HughParsonage <hugh.parsonage@gmail.com>
2021-05-24 22:40:01 +1000

    Document minmax, rationalize string equals

--- tests/testthat/test_string_equals.R
+++ tests/testthat/test_string_equals.R
@@ -8,6 +8,9 @@ test_that("string_equals", {
   zw <- c("svGwr", "Jlvu2", "o8u2a", "ToOWY", "fXg9I", "iDc4K", "LKVok",
           "z12V4", "VYRWX", "A0rm_")
 
+  expect_true(string_equals("", ""))
+  expect_false(string_equals(character(2), c("", " a")))
+  expect_true(string_equals(rep("a", 10), "a"))
   expect_true(string_equals(x, y))
   expect_false(string_equals(x, zz))
   expect_false(string_equals(x, zw))
