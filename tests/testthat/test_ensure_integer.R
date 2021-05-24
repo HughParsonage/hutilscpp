00463e1f3b333c26d7add90b749c37f779364b51
HughParsonage <hugh.parsonage@gmail.com>
2021-05-24 14:38:01 +1000

    Test fully ensure_integer

---
+++ tests/testthat/test_ensure_integer.R
@@ -0,0 +1,8 @@
+test_that("ensure_integer", {
+  expect_error(ensure_integer(5.5))
+  expect_error(ensure_integer("5"))
+  expect_warning(ensure_integer(5.5, on_failure = "warn"))
+  expect_error(ensure_integer(5e9))
+  expect_identical(ensure_integer(5), 5L)
+  expect_identical(ensure_integer(5L), 5L)
+})
