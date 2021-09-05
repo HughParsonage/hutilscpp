146edd26269868c74e2f8006a227c44f7fd3cbc0
HughParsonage <hugh.parsonage@gmail.com>
2021-09-05 23:37:45 +1000

    covr

--- inst/tinytest/test-check_omp.R
+++ inst/tinytest/test-check_omp.R
@@ -3,6 +3,7 @@ check_omp <- hutilscpp:::check_omp
 expect_error(check_omp(NA), "logical")
 expect_error(check_omp(NA_integer_), "nThread")
 expect_error(check_omp(5.5), "whole number")
+expect_equal(check_omp(1), 1)
 
 if (hutilscpp:::has_openmp() && requireNamespace("parallel", quietly = TRUE)) {
   expect_error(check_omp(parallel::detectCores() + 1L), "nThread")
