b28c155702ba45381cb93b22b9e92236d7dccad2
HughParsonage <hugh.parsonage@gmail.com>
2022-04-02 20:19:00 +1100

    uminus rationalize

--- src/uminus.c
+++
@@ -1,9 +0,0 @@
-#include "hutilscpp.h"
-
-SEXP Cuminus(SEXP x, SEXP y) {
-  unsigned int xx = asInteger(x);
-  unsigned int yy = asInteger(y);
-  unsigned int u = xx - yy;
-  Rprintf("%u\n", u);
-  return ScalarInteger(u);
-}
