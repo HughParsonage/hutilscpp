64b6a3db43f14ccab28b728eeb1347fb1c41d2ee
HughParsonage <hugh.parsonage@gmail.com>
2022-04-02 13:47:20 +1100

    Remove cf_xlen

--- src/cf_xlen.c
+++ src/cf_xlen.c
@@ -1,20 +1,5 @@
 #include "hutilscpp.h"
 
-int cf_xlen(SEXP x, SEXP y) {
-  R_xlen_t Ny = xlength(y);
-  if (Ny == 1) {
-    return CF_LEN_1;
-  }
-  if (Ny == 2) {
-    return CF_LEN_2;
-  }
-  if (Ny == xlength(x)) {
-    return CF_LEN_N;
-  }
-  return 0;
-
-}
-
 int op_xlen2(int o) {
   return o == OP_BW || o == OP_BC || o == OP_BO || o == OP_WB;
 }
