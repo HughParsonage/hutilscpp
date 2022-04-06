880e761127cd13dadae773462caa299e5de90c7f
HughParsonage <hugh.parsonage@gmail.com>
2022-04-06 16:51:04 +1000

    Cover line 56

--- inst/tinytest/test_character2integer.R
+++ inst/tinytest/test_character2integer.R
@@ -74,4 +74,4 @@ expect_equal(Comma(c(5, 4, 5.5), big.mark = ",", digits = 1L),
 expect_equal(character2integer(c("5.0", "4.0", "5.5"), allow.double = TRUE,
                                na.strings = "na"),
              c(5, 4, 5.5))
-
+expect_error(character2integer(c("5", "55", "55.005"), allow_double = FALSE))
