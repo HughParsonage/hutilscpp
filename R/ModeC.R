f409be1ed095ca1c268d289284c1de915f6d9206
HughParsonage <hugh.parsonage@gmail.com>
2022-04-07 01:05:14 +1000

    Use option not .option for consistency

--- R/ModeC.R
+++ R/ModeC.R
@@ -3,7 +3,7 @@
 #' @param nThread Number of threads to use.
 #' @param .range_fmatch If the range of \code{x} differs by more than
 #' this amount, the mode will be calculated via \code{fmatchp}.
-#' @param .option \code{integer(1)} Handle exceptional cases:
+#' @param option \code{integer(1)} Handle exceptional cases:
 #' \describe{
 #' \item{0}{Returns \code{NULL} quietly.}
 #' \item{1}{Returns an error if the mode cannot be calculated.}
@@ -20,11 +20,11 @@
 ModeC <- function(x,
                   nThread = getOption("hutilscpp.nThread", 1L),
                   .range_fmatch = 1e9,
-                  .option = 1L) {
+                  option = 1L) {
   xminmax <- minmax(x, nThread = nThread)
   if (anyNA(xminmax) ||
-      xminmax[1] + .range_fmatch <= xminmax[2] ||
-      !is.integer(x)) {
+      !is.integer(x) ||
+      xminmax[1] + .range_fmatch <= xminmax[2]) {
     fx <- fmatchp(x, x, nThread = nThread)
     mode_fx <- .Call("C_Mode", fx, nThread, NULL, PACKAGE = "hutilscpp")
     return(x[mode_fx])
@@ -32,8 +32,8 @@ ModeC <- function(x,
   ans <- .Call("C_Mode", x, nThread, xminmax, PACKAGE = "hutilscpp")
   # nocov start
   if (is.null(ans)) {
-    if (.option) {
-      switch(.option,
+    if (option) {
+      switch(option,
              stop("Unable to calculate ModeC."),
              {
                warning("Unable to calculate ModeC, falling back to hutils::Mode")
