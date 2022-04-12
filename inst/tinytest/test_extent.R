89a257708b7158f473db287d7129341c07f883ef
HughParsonage <hugh.parsonage@gmail.com>
2022-04-12 23:12:32 +1000

    Test int x but diam exeeds INTMAX

--- inst/tinytest/test_extent.R
+++ inst/tinytest/test_extent.R
@@ -1,4 +1,7 @@
 library(hutilscpp)
 expect_equal(diam(c(0L, 1L)), 1L)
+
+# exceeds INTMAX
+expect_equal(diam(c(-1L, 2147483647L)), 2147483648)
 expect_true(thinner(0:5, 8L))
 expect_false(thinner(0:5, 2L))
