38641588a760531413a24c3877a128fcdd322d0a
HughParsonage <hugh.parsonage@gmail.com>
2022-04-13 14:31:52 +1000

    Test pcg_hash

---
+++ inst/tinytest/test_pcg_hash.R
@@ -0,0 +1,6 @@
+x <- hutilscpp:::pcg_hash64(10L)
+expect_equal(length(x))
+expect_true(is.integer(x))
+ya <- hutilscpp:::pcg_hash64(10L, 1:100)
+yb <- hutilscpp:::pcg_hash64(10L, 1:100)
+expect_equal(ya, yb)
