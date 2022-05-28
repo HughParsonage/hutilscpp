d3556082164078d16f7e33c11721969568409137
HughParsonage <hugh.parsonage@gmail.com>
2022-05-29 01:26:25 +1000

    Add tests

--- inst/tinytest/test_pcg_hash.R
+++ inst/tinytest/test_pcg_hash.R
@@ -8,3 +8,15 @@ if (!hutilscpp:::is64bit()) {
   yb <- ya
 }
 expect_equal(ya, yb)
+pcg_hash <- hutilscpp:::pcg_hash
+x <- hutilscpp:::pcg_hash(105L)
+expect_equal(length(x), 105)
+x <- hutilscpp:::pcg_hash(105, raw_result = TRUE)
+expect_true(is.raw(x))
+if (tinytest::at_home()) {
+  expect_equal(length(pcg_hash(106, nThread = 2L)), 106)
+  expect_equal(length(pcg_hash(106, nThread = 52L)), 106)
+  expect_equal(pcg_hash(106, r = 1:200), pcg_hash(106, r = 1:200))
+}
+
+
